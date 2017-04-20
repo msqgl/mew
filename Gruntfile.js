6991cd94fda26816f540b9e37a9688cbf344c779
Michele Squeglia <squeglia.michele@gmail.com>
2017-04-20 16:01:04 +0200

    aggiornato grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,36 +4,25 @@ module.exports = function (grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
-        clean: ['dist'],
+        clean: ['dist', 'src/css', 'src/js/*.min.js*'],
 
-        concat: {
-            js: {
-                src: ['src/js/*.js'],
-                dest: 'dist/main.js'
-            },
-            css: {
-                src: ['src/css/*.css'],
-                dest: 'dist/main.css'
+        less: {
+            build: {
+                src: 'src/less/*.less',
+                dest: 'src/css/style.min.css',
+                options: {
+                    compress: true
+                }
             }
         },
 
         uglify: {
             js: {
                 files: {
-                    'dist/main.min.js': ['dist/main.js']
+                    'src/js/main.min.js': 'src/js/main.js',
+                    'src/js/maps.min.js': 'src/js/maps.js',
+                    'src/js/owl-carousel2.min.js': 'src/js/owl-carousel2.js'
                 }
-            }
-        },
-
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: 'dist',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'dist',
-                    ext: '.min.css'
-                }]
             }
         },
 
@@ -43,12 +32,25 @@ module.exports = function (grunt) {
                     {expand: true, cwd: 'bower_components/bootstrap/dist/', src: ['**'], dest: 'dist/bootstrap'},
                     {expand: true, cwd: 'bower_components/jquery/dist/', src: ['**'], dest: 'dist/jquery'},
                     {expand: true, cwd: 'bower_components/owl.carousel/dist/', src: ['**'], dest: 'dist/owl.carousel'},
-                    {expand: true, cwd: 'bower_components/font-awesome/css/', src: ['**'], dest: 'dist/font-awesome/css'},
-                    {expand: true, cwd: 'bower_components/font-awesome/fonts/', src: ['**'], dest: 'dist/font-awesome/fonts'},
-                    {expand: true, cwd: 'bower_components/gmaps/', src: ['gmaps.*'], dest: 'dist/gmaps'}
+                    {
+                        expand: true,
+                        cwd: 'bower_components/font-awesome/css/',
+                        src: ['**'],
+                        dest: 'dist/font-awesome/css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/font-awesome/fonts/',
+                        src: ['**'],
+                        dest: 'dist/font-awesome/fonts'
+                    },
+                    {expand: true, cwd: 'bower_components/gmaps/', src: ['gmaps.*'], dest: 'dist/gmaps'},
+                    {expand: true, cwd: 'src/js', src: ['*.min.js'], dest: 'dist/custom/js'},
+                    {expand: true, cwd: 'src/css', src: ['*.min.css'], dest: 'dist/custom/css'}
                 ]
             }
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -56,6 +58,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'cssmin', 'copy']);
+    grunt.registerTask('default', ['clean', 'less', 'uglify', 'copy']);
 };
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    var bodyId = $("body").prop("id");
+    var basePath = "dist/custom/js/";
+
+    switch (bodyId) {
+        case 'js_maps':
+            $.getScript(basePath + "maps.min.js");
+            break;
+        case 'js_carousel':
+            $.getScript(basePath + "owl-carousel2.min.js");
+            break;
+    }
+
+});
