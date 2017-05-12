76df5c3a5bbb06f1fdab8c2718a0b4b6ac80484c
Michele Squeglia <squeglia.michele@gmail.com>
2017-05-12 11:51:37 +0200

    updated pages and menu

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $("#getting-started").countdown("2017/09/22", function(event) {
+    $("#getting-started").countdown("2017/09/22 15:30:00", function(event) {
         $(this).text(
             event.strftime('%D giorni %H:%M:%S')
         );
