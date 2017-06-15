64f60afa850b2bd0703e56f1d6a45367306cdca1
Michele Squeglia <squeglia.michele@gmail.com>
2017-06-15 17:19:13 +0200

    new management for jumbotron background

--- src/js/wedding-list.js
+++ src/js/wedding-list.js
@@ -8,10 +8,27 @@ $(function () {
     });
 
 
-    $("#gift-modal-button").on("click tap", function() {
-        $(".step-1").fadeOut(function() {
+    $("#gift-modal-button").on("click tap", function () {
+        $(".step-1").fadeOut(function () {
             $(".step-2").fadeIn();
         });
     });
 
+    function changeBackground() {
+        $('.jumbotron').fadeTo('slow', 0.3, function () {
+            var seychellesClass = "bg-seychelles";
+            var japanClass = "bg-japan-lantern";
+            if ($(this).hasClass(seychellesClass)) {
+                $(this).removeClass(seychellesClass);
+                $(this).addClass(japanClass);
+            } else {
+                $(this).removeClass(japanClass);
+                $(this).addClass(seychellesClass);
+            }
+        }).fadeTo('slow', 1);
+    }
+
+    setInterval(function () {
+        changeBackground()
+    }, 5000)
 });
