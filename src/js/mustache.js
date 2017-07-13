570c68b0ac9642a3821662ccaced037d508eb095
Michele Squeglia <squeglia.michele@gmail.com>
2017-07-13 10:18:49 +0200

    Updated trip page.

--- src/js/mustache.js
+++ src/js/mustache.js
@@ -3,13 +3,13 @@ $(function () {
     function loadItemList(json) {
         var template = $('#template').html();
         Mustache.parse(template);
+        json['data'][ json['data'].length - 1 ].last = true;
         var rendered = Mustache.render(template, json);
         $('#target').prepend(rendered);
         $.each($(".card-container"), function () {
             var button = $(this).find("button");
             var totalPrice = $(button).data("total-price");
             var consumedPrice = $(button).data("consumed-price");
-            console.log("totalPrice " + totalPrice + ", consumedPrice " + consumedPrice);
             if (totalPrice <= consumedPrice) {
                 var greenCheck = $(this).find(".green-check");
                 $(greenCheck).removeClass("hidden");
@@ -19,7 +19,7 @@ $(function () {
     }
 
     function modalLoadingError() {
-        $("#modalError").modal();
+        $("#gift-error").modal();
     }
 
     RestModule.getAllGift(loadItemList, modalLoadingError);
