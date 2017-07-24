92b7d2e186cad1742be1c9a327d285732c43cdae
Michele Squeglia <squeglia.michele@gmail.com>
2017-07-24 17:22:43 +0200

    Updated countdown management.

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,9 +1,23 @@
 $(function () {
 
-    $("#getting-started").countdown("2017/09/22 15:30:00", function(event) {
-        $(this).text(
+    var $countdownSelector = $("#getting-started");
+    var $jsCountdown = $("#js-countdown");
+    var $H2 = $jsCountdown.find("h2");
+
+    var weddingDateTime = "2017/07/24 17:22:00";
+
+    var updateCountdown = function (event) {
+        $countdownSelector.text(
             event.strftime('%D giorni %H:%M:%S')
         );
-    });
+    };
 
+    var finishCountdown = function () {
+        $H2.hide()
+    };
+
+    $countdownSelector.countdown(weddingDateTime)
+        .on("update.countdown", updateCountdown)
+        .on("finish.countdown", finishCountdown)
+    ;
 });
