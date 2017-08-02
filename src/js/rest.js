fb75010e4cd45338a0a51b7f3351fd234470ab9a
Michele Squeglia <squeglia.michele@gmail.com>
2017-08-03 00:30:06 +0200

    JS refact.

--- src/js/rest.js
+++ src/js/rest.js
@@ -42,8 +42,28 @@ var RestModule = function () {
         });
     }
 
-    return {
-        getAllGift: getAllGift,
-        saveGiftMsg: saveGiftMsg
+    function loadItemList(json) {
+        var template = $('#template').html();
+        Mustache.parse(template);
+        json['data'][ json['data'].length - 1 ].last = true;
+        var rendered = Mustache.render(template, json);
+        $('#target').prepend(rendered);
+        $.each($(".card-container"), function () {
+            var button = $(this).find("button");
+            var totalPrice = $(button).data("total-price");
+            var consumedPrice = $(button).data("consumed-price");
+            if (totalPrice <= consumedPrice) {
+                var greenCheck = $(this).find(".green-check");
+                $(greenCheck).removeClass("hidden");
+                $(button).hide();
+            }
+        });
     }
+
+    function modalLoadingError() {
+        $("#gift-error").modal();
+    }
+
+    getAllGift(loadItemList, modalLoadingError);
+
 }();
